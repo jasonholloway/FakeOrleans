9adb4e533b73abe914b9f7a0f5421cc3f1f2e48f
Jason Holloway <miceeatnicerice@hotmail.com>
2016-08-22 18:02:28 +0100

    Separated out ActivationSite

--- MockOrleans.Tests/ActivationSiteTests.cs
+++ MockOrleans.Tests/ActivationSiteTests.cs
@@ -13,60 +13,6 @@ using System.Threading.Tasks;
 namespace MockOrleans.Tests
 {
 
-    public class DeactivatedException : Exception { }
-
-
-    public interface IActivation
-    {        
-        Task<TResult> Perform<TResult>(Func<IActivation, Task<TResult>> fn);
-    }
-
-
-    public interface IActivationProvider
-    {
-        IActivation GetActivation();
-    }
-
-
-        
-    public interface IActivationSite
-    {
-        Task<TResult> Dispatch<TResult>(Func<IActivation, Task<TResult>> fn);
-    }
-
-
-
-    public class ActivationSite : IActivationSite
-    {
-        readonly IActivationProvider _actProv;
-
-        IActivation _act = null;
-        object _sync = new object();
-        
-        public ActivationSite(IActivationProvider actProv) {
-            _actProv = actProv;
-        }
-                
-        public Task<TResult> Dispatch<TResult>(Func<IActivation, Task<TResult>> fn) 
-        {
-            IActivation act = null;
-
-            lock(_sync) {
-                act = _act ?? (_act = _actProv.GetActivation());
-            }
-            
-            try {
-                return act.Perform(fn);
-            }
-            catch(DeactivatedException) {
-                lock(_sync) _act = null;
-                return Dispatch(fn);
-            }
-        }
-        
-    }
-    
-
     [TestFixture]
     public class ActivationSiteTests
     {
---
+++ MockOrleans/Grains/ActivationSite.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockOrleans.Grains
+{
+
+    public class DeactivatedException : Exception { }
+
+
+    public interface IActivation
+    {
+        Task<TResult> Perform<TResult>(Func<IActivation, Task<TResult>> fn);
+    }
+
+
+    public interface IActivationProvider
+    {
+        IActivation GetActivation();
+    }
+
+
+
+    public interface IActivationSite
+    {
+        Task<TResult> Dispatch<TResult>(Func<IActivation, Task<TResult>> fn);
+    }
+
+
+
+    public class ActivationSite : IActivationSite
+    {
+        readonly IActivationProvider _actProv;
+
+        IActivation _act = null;
+        object _sync = new object();
+
+        public ActivationSite(IActivationProvider actProv) {
+            _actProv = actProv;
+        }
+
+        public Task<TResult> Dispatch<TResult>(Func<IActivation, Task<TResult>> fn) {
+            IActivation act = null;
+
+            lock(_sync) {
+                act = _act ?? (_act = _actProv.GetActivation());
+            }
+
+            try {
+                return act.Perform(fn);
+            }
+            catch(DeactivatedException) {
+                lock(_sync) _act = null;
+                return Dispatch(fn);
+            }
+        }
+
+    }
+
+
+}
